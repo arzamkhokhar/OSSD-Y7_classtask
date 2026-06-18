c0e2f1a943243f40831163cbee64aecaf8d92a4a
arzamkhokhar <arzamameer34@gmail.com>
2026-06-18 09:03:18 +0500

    Complete Week 12 SQLite login task

--- Week12/sqlite.py
+++ Week12/sqlite.py
@@ -1,14 +1,28 @@
 import sqlite3
 
+con = sqlite3.connect("project.db")
+cursor = con.cursor()
 
-con=sqlite3.connect("project.db")
-cursor=con.cursor()
+V1 = input("Enter username: ")
+V2 = input("Enter email: ")
+V3 = input("Enter password: ")
 
-cursor.execute()  # add create table query
+t = (V1, V2, V3)
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS LOGIN_INFO(
+    USERNAME TEXT PRIMARY KEY,
+    EMAIL TEXT NOT NULL,
+    PASSWORD TEXT NOT NULL
+)
+""")
+
+cursor.execute(
+    "INSERT INTO LOGIN_INFO (USERNAME, EMAIL, PASSWORD) VALUES (?, ?, ?)",
+    t
+)
+
+print("Data inserted successfully")
 
 con.commit()
 con.close()
-
-
-
-
